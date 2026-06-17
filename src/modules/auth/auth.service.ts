7667c576f7bc75479ef51c0bd60b25ba205d6412
aharonguedez <aharon.guedez23@gmail.com>
2026-06-17 15:11:00 -0400

    cambio de libreria

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,7 +6,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { PrismaService } from '../../prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
